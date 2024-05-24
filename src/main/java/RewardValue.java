94296f89bb2e2fd454a28baa2651f8a743819d20
shubhamshah15 <you@example.com>
2024-05-24 06:31:31 -0700

    Added unit test cases to convert from cash to miles and vice versa.

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,7 +1,7 @@
 public class RewardValue {
     private double cashValue;
     private double milesValue;
-    private static final double MILES_TO_CASH_RATE = 0.0035;
+    public static final double MILES_TO_CASH_RATE = 0.0035;
 
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
